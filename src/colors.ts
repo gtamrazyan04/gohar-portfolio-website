a58a4e3a926c3aff184b7a40a42c5f4e8dce9267
gtamrazyan04 <gtamrazyan@ethz.ch>
2025-01-11 12:55:32 +0100

    first commit with navigation

---
+++ src/colors.ts
@@ -0,0 +1,48 @@
+const theme = {
+  colors: {
+    blue: {
+      primary: "#0056D6",
+      light: "#85C1E9",
+      dark: "#003C8F",
+      accent: "#0099FF",
+      veryLight: "#EAF4FE",
+    },
+    gray: {
+      dark: "#3D3D3D",
+      medium: "#6E6E6E",
+      light: "#D3D3D3",
+      veryLight: "#F5F5F5",
+    },
+    basic: {
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+    red: {
+      primary: "#D32F2F",
+      light: "#F28B82",
+      dark: "#9A0007",
+    },
+    green: {
+      primary: "#388E3C",
+      light: "#81C784",
+      dark: "#2E7D32",
+    },
+    yellow: {
+      primary: "#FBC02D",
+      light: "#FFEB3B",
+      dark: "#F57F17",
+    },
+    orange: {
+      primary: "#F57C00",
+      light: "#FFB74D",
+      dark: "#E65100",
+    },
+    purple: {
+      primary: "#8E24AA",
+      light: "#BA68C8",
+      dark: "#6A1B9A",
+    },
+  },
+};
+
+export default theme;
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { CSSTransition } from "react-transition-group";
+
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <nav className="navigation">
+      {/* Logo */}
+      <div className="navigation_left">
+        <a href="#home" className="navigation_left-link">
+          <span className="logo_name">Gohar Tamrazyan</span>
+        </a>
+      </div>
+
+      {/* Navigation Links */}
+      <ul className="navigation_right">
+        {["home", "about", "work", "skills", "testimonials", "contact"].map(
+          (item) => (
+            <li key={item} className="navigation_link-item">
+              <a href={`#${item}`} className="navigation_link">
+                {item}
+              </a>
+            </li>
+          )
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
